5fd98135eb45accee619e4ed199dc21bad6ccd65
Me <edu.kaelteritter@yandex.ru>
2025-08-08 11:56:37 +0400

    Реализиция подсчета специальной цепочки XOR
    Обновлен core/rules.py: создан метод exclude_points_xor

--- core/rules.py
+++ core/rules.py
@@ -5,7 +5,7 @@ import logging
 
 from core.board import Board
 from core.elements import Element
-from core.exceptions import TokenInvalidError
+from core.exceptions import CellOutOfBorderError, TokenInvalidError
 from core.operands import FalseOperand, Operand, TrueOperand
 from core.players import Player
 from core.tokens import AND, IMP, OR, XOR, Token
@@ -53,7 +53,7 @@ class ThunderTruthRules(Rules):
 
     def is_board_full(self, board: Board) -> bool:
         status = all(
-            board.get_cell(row, col).is_empty == False
+            not board.get_cell(row, col).is_empty
             for row in range(1, board.get_size() + 1) 
             for col in range(1, board.get_size() + 1)
             )
@@ -76,7 +76,7 @@ class ThunderTruthRules(Rules):
         if not self.is_board_full(board):
             return None
 
-        players_by_points = list(sorted(players, key=lambda p: p.get_points()))
+        players_by_points = list(sorted(players, key=lambda p: -p.get_points()))
         player1, player2 = players_by_points[0], players_by_points[1]
 
         if player1.get_points() == player2.get_points():
@@ -92,6 +92,73 @@ class ThunderTruthRules(Rules):
             return False
         logger.debug(f'Проверка на владельца: токен {token.get_id()} принадлежит игроку {player.get_id()}')
         return True
+    
+    def _is_chain_on_board(self, board: Board, chain: list[tuple[int, int]]) -> bool:
+        if not all(1 <= _row <= board.get_size() and 1 <= _col <= board.get_size() for _row, _col in chain):
+            logger.debug(f'Не все клетки в цепочке {chain} внутри игрового поля. Пропуск...')
+            return False
+        return True
+    
+    def _has_chain_valid_types(self, elements, types):
+        if not all(isinstance(element, instance) for element, instance in zip(elements, types)):
+            logger.debug(
+                f'Не все клетки в цепочке верных типов: '
+                f'Действительный/Ождиаемый типы элементов: '
+                f'{[(type(element), instance) for element, instance in zip(elements, types)]}'
+                )
+            return False
+        return True
+        
+    def exclude_points_xor(self, board: Board, row: int, col: int) -> tuple[Player, Player] | None:
+        """
+        Ищет "обнуляющую" цепочку из 3 операндов и 2 токенов (один - соперника, второй - свой XOR).
+        В случае успеха возвращет двух игроков. Первый - соперник, второй - текущий игрок с XOR
+        """
+        chains = [
+            [(row, col-3), (row, col-2), (row, col-1), (row, col), (row, col+1)],  # по горизонтали
+            [(row-3, col), (row-2, col), (row-1, col), (row, col), (row+1, col)],  # по вертикали
+        ]
+        expected = [Operand, Token, Operand, XOR, Operand]
+
+        for chain in chains:
+            # Проверяем, что все координат в границах
+            if not self._is_chain_on_board(board, chain):
+                continue
+            
+            elements = [board.get_cell_buffered(_row, _col).value for _row, _col in chain]
+            
+            # Все элементы составляют цепочку op1, token1, op2, token2, op3
+            if not self._has_chain_valid_types(elements, expected):
+                continue
+
+            op1, token1, op2, token2, op3 = elements
+
+            # Токены принадлежат: один - сопернику, второй - делающему ход
+            if self.is_token_owner(token2.get_owner(), token1):
+                continue
+            
+
+            op1_op2 = TrueOperand() if token1.evaluate(op1, op2) else FalseOperand()
+            result = token2.evaluate(op1_op2, op3)
+
+            if result:
+                logger.debug(
+                f"Цепочка XOR: {op1.to_string()} {token1.to_string()} {op2.to_string()} "
+                f"{token2.to_string()} {op3.to_string()} -> {result}"
+                )
+                return token1.get_owner(), token2.get_owner()
+            
+            else:
+                logger.debug(
+                    f"Цепочка XOR: {op1.to_string()} {token1.to_string()} {op2.to_string()} "
+                    f"{token2.to_string()} {op3.to_string()} не валидна"
+                    )
+
+        return None
+
+
+            
+
         
     def count_points(self, board: Board, row: int, col: int) -> int:
         """
