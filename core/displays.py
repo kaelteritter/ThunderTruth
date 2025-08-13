7043fb50d41994c872e78331792a935d291a47fa
Me <edu.kaelteritter@yandex.ru>
2025-08-13 17:26:01 +0400

    Совершенстсование консольного вывода
    - Обновлен core/displays.py: создан специальный
    ConsoleShowMixin с методами boldify, colorize
    для удобной раскраски, а также специальный
    метод для отображения координат на полях вместо '#'

--- core/displays.py
+++ core/displays.py
@@ -2,10 +2,11 @@
 from abc import ABC, abstractmethod
 import logging
 
-from colorama import Style
+from colorama import Fore, Style
 
 from core import settings, utils
 from core.board import Board
+from core.elements import Stub
 from core.players import Player
 
 logger = logging.getLogger(__name__)
@@ -43,17 +44,34 @@ class Display(ABC):
         pass
 
 
+class ConsoleShowMixin:
+    def _substitute_to_string(self, element, row, col):
+        if isinstance(element, Stub):
+            if col == row == 0:
+                return element.to_string()
+            if col == 0:
+                return self.boldify(self.colorize(row, Fore.LIGHTMAGENTA_EX))
+            if row == 0:
+                return self.boldify(self.colorize(col, Fore.LIGHTMAGENTA_EX))
+            return self.boldify(element.to_string())
+        return element.to_string()
+    
+    def boldify(self, string: str):
+        return f'{Style.BRIGHT}{string}{Style.RESET_ALL}'
+    
+    def colorize(self, string: str, color):
+        return f'{color}{string}{Style.RESET_ALL}'
 
 
-class ConsoleDisplay(Display):
+class ConsoleDisplay(Display, ConsoleShowMixin):
     def display_board(self, board: Board) -> None:
         output_board = []
 
         for row in range(board.get_size_buffered()):
             row_cells = []
             for col in range(board.get_size_buffered()):
-                element = board.get_cell_buffered(row, col).value.to_string()
-                row_cells.append(element)
+                element = board.get_cell_buffered(row, col).value
+                row_cells.append(self._substitute_to_string(element, row, col))
 
             output_board.append(' | '.join(row_cells))
         output_board = '\n'.join(output_board)
@@ -87,7 +105,7 @@ class ConsoleDisplay(Display):
         print(f"Ход игрока {player.get_color()}{Style.BRIGHT}{player.name}{Style.RESET_ALL}")
         
     def show_token_available(self, player: Player) -> None:
-        print(f'Ваши токены: {", ".join(token.to_string() for token in player.tokens)}')
+        print(f'Ваши токены: [{", ".join(token.to_string() for token in player.tokens)}]')
 
     def show_next_move_notification(self) -> None:
         print(f'{Style.DIM}Следующий ход...{Style.RESET_ALL}')
